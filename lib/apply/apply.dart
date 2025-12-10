1ba53142f72780cc97dac1640e20d610be96951e
leejia0324 <hx.un0707@gmail.com>
2025-12-10 23:18:19 +0900

    DMS 신청페이지 구현

---
+++ lib/apply/apply.dart
@@ -0,0 +1,310 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class ApplyPage extends StatefulWidget {
+  const ApplyPage({super.key});
+
+  @override
+  State<ApplyPage> createState() => _ApplyPageState();
+}
+
+class _ApplyPageState extends State<ApplyPage>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController = TabController(
+    length: 2,
+    vsync: this,
+    initialIndex: 0,
+  );
+
+  Widget _tabBar() {
+    return TabBar(
+      overlayColor: WidgetStatePropertyAll(
+        Colors.transparent
+      ),
+      padding: EdgeInsets.only(top: 40),
+      controller: tabController,
+      labelColor: Colors.white,
+      labelStyle: TextStyle(fontWeight: FontWeight.w900),
+      indicatorColor: Colors.white,
+      indicatorSize: TabBarIndicatorSize.tab,
+      tabs: [
+        Tab(text: '신청'),
+        Tab(text: '투표'),
+      ],
+    );
+  }
+
+  Widget _tabBarView() {
+    return TabBarView(
+      controller: tabController,
+      children: [
+        Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsetsGeometry.only(top: 15),
+                child: Center(
+                  child: Container(
+                    width: 350,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Color(0xff1f1f1f),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsGeometry.only(
+                                top: 20,
+                                left: 20,
+                              ),
+                              child: Text(
+                                '잔류 신청',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 150, top: 20),
+                              child: Container(
+                                width: 90,
+                                height: 40,
+                                decoration: BoxDecoration(
+                                  color: Color(0xffa9d1ff),
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '금요일 귀가',
+                                    style: TextStyle(
+                                      color: Color(0xff015fc2),
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15),
+                          child: Container(
+                            width: 310,
+                            child: Flexible(
+                              child: Text(
+                                '주말 기숙사 잔류 여부를 확인하고 잔류 신청을 통해서 잔류 또는 귀가를 신청해 보세요.',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Container(
+                            width: 310,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xff008dff),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '잔류 신청하기',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.only(top: 10),
+                child: Center(
+                  child: Container(
+                    width: 350,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Color(0xff1f1f1f),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsGeometry.only(
+                                top: 20,
+                                left: 20,
+                              ),
+                              child: Text(
+                                '외출 신청',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15, right: 20),
+                          child: Container(
+                            width: 290,
+                            child: Flexible(
+                              child: Text(
+                                '기숙사에서 생활하는 동안 밖에 다녀올 일이 있다면 외출을 신청해 보세요.',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Container(
+                            width: 310,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xff008dff),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '외출 신청하기',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsGeometry.only(top: 10),
+                child: Center(
+                  child: Container(
+                    width: 350,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Color(0xff1f1f1f),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsetsGeometry.only(
+                                top: 20,
+                                left: 20,
+                              ),
+                              child: Text(
+                                '봉사 신청',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 15, right: 130),
+                          child: Container(
+                            width: 180,
+                            child: Flexible(
+                              child: Text(
+                                '학생들이 직접 봉사 활동을 신청하고 신청한 활동에 참여할 수 있습니다.',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Container(
+                            width: 310,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xff008dff),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                '봉사 신청하기',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w900,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          child: Center(
+            child: Text('투표 기간이 아닙니다.', style: TextStyle(color: Colors.white)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff111111),
+
+      // appBar: AppBar(title: Text('data'), backgroundColor: Color(0xf1f1f1),),
+      body: Column(
+        children: [
+          _tabBar(),
+          Expanded(child: _tabBarView()),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/mypage/mypage.dart
@@ -0,0 +1,275 @@
+import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+
+class MyPageScreen extends StatelessWidget {
+  const MyPageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff111111),
+      body: Column(
+        children: [
+          Padding(padding: EdgeInsets.only(top: 50)),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(right: 40),
+                        child: Text(
+                          '2114 정지윤',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        // 여
+                        width: 35,
+                        height: 30,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Color(0xfffed2d2),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '여',
+                            style: TextStyle(
+                              color: Color(0xffca4349),
+                              fontWeight: FontWeight.w900,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(padding: EdgeInsets.only(left: 210)),
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                    child: Text(
+                      '대덕소프트웨어마이스터고등학교',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(width: 90),
+
+              Padding(padding: EdgeInsets.only(bottom: 100)),
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(150),
+                  color: Color(0xffdddddd),
+                ),
+                child: Icon(Icons.person, size: 70, color: Colors.white),
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                width: 380,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color(0xffa9d1ff),
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '문구가 없습니다.',
+                    style: TextStyle(
+                      color: Color(0xff1f1f1f),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 110, left: 5)),
+              Container(
+                width: 180,
+                height: 85,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xff2463a7), width: 2),
+                ),
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Padding(
+                      padding: EdgeInsets.only(right: 120),
+                      child: Text(
+                        '상점',
+                        style: TextStyle(
+                          color: Color(0xff2463a7),
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 5)),
+                    Padding(
+                      padding: EdgeInsets.only(left: 120),
+                      child: Text(
+                        '52',
+                        style: TextStyle(
+                          color: Color(0xff2463a7),
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 10),
+              Container(
+                width: 180,
+                height: 85,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  border: Border.all(color: Color(0xffc04041), width: 2),
+                ),
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(top: 10)),
+                    Padding(
+                      padding: EdgeInsets.only(right: 120),
+                      child: Text(
+                        '벌점',
+                        style: TextStyle(
+                          color: Color(0xffc04041),
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 5)),
+                    Padding(
+                      padding: EdgeInsets.only(left: 130),
+                      child: Text(
+                        '2',
+                        style: TextStyle(
+                          color: Color(0xffc04041),
+                          fontSize: 25,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 370,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Color(0xff1f1f1f),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 15, left: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '상벌점 내역 확인',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
+                ),
+                Divider(indent: 15, endIndent: 15, color: Color(0xff686868)),
+                Padding(
+                  padding: EdgeInsets.only(top: 5, left: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '비밀번호 변경',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
+                ),
+                Divider(indent: 15, endIndent: 15, color: Color(0xff686868)),
+                Padding(
+                  padding: EdgeInsets.only(top: 5, left: 20),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      '알림 설정',
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Container(
+              width: 370,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xff1f1f1f),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    '로그아웃',
+                    style: TextStyle(
+                      color: Color(0xffc04041),
+                      fontSize: 17,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Container(
+              width: 370,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color(0xff1f1f1f),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text(
+                    '회원 탈퇴',
+                    style: TextStyle(
+                      color: Color(0xffc04041),
+                      fontSize: 17,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
